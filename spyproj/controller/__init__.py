daafbab39cd7b84ef21e0e1e1a94c2bf0f56ff46
Gopi <krishnar8@gmail.com>
2022-10-13 18:16:42 +0530

    cmmit

--- spyproj/controller/__init__.py
+++ spyproj/controller/__init__.py
@@ -1,6 +1,6 @@
+from spyproj.controller import camera
+from spyproj.controller import historyroute
+from spyproj.controller import userroute
 from flask import Flask
 
 app = Flask(__name__)
-
-from spyproj.controller import userroute
-from spyproj.controller import historyroute
--- spyproj/controller/userroute.py
+++ spyproj/controller/userroute.py
@@ -4,8 +4,7 @@ from spyproj import app
 from spyproj.repository.userdetails_repository import UserDetails
 from spyproj.model.user_details_data import UserDetailsData
 from flask import jsonify
-import json
-from bson import json_util
+
 
 
 @app.route('/')
@@ -19,12 +18,10 @@ def users():
         # find method returns cursor object
         usercursor = UserDetails.get_users()
         if usercursor:
-            print("*********")
             # convert cursor to list of dictionaries
             userlist = list(usercursor)
-            print("**aaa***abcd*", userlist)
             users = []
-           
+
             for user in userlist:
                 password = ''
                 location = ''
@@ -33,7 +30,7 @@ def users():
                 # if password is None:
                 if 'password' in user:
                     password = user['password']
-                
+
                 if 'location' in user:
                     location = user['location']
 
@@ -47,7 +44,7 @@ def users():
             # convert into json
             # json_data = json.dumps(xyz, default = str)
             json_data = jsonify(users)
-            print("---------", str(json_data))
+
             # return Response(response="suceess", status=200, mimetype="application/json")
             return json_data
         else:
@@ -96,11 +93,11 @@ def getUserByName():
 def createuser():
     try:
         print('hi')
-
         newDataReq = request.json["newData"]
 
         # pass the primary key as filter in order to update document based on it
-        usercursor = UserDetails.get_userbyid({'username':newDataReq["username"] })
+        usercursor = UserDetails.get_userbyid(
+            {'username': newDataReq["username"]})
 
         # if already user is exist then skip the create user
         userlist = list(usercursor)
@@ -118,29 +115,29 @@ def createuser():
         return Response(response="Failure", status=500, mimetype="application/json")
 
 
-@app.route('/updateUserByName', methods=['GET','POST'])
+@app.route('/updateUserByName', methods=['GET', 'POST'])
 def updateUserByName():
     try:
-
+        print("****==>", request.json["updateData"])
         updateDataReq = request.json["updateData"]
 
         # pass the primary key as filter in order to update document based on it
-        usercursor = UserDetails.get_userbyid({'username':updateDataReq["username"] })
+        usercursor = UserDetails.get_userbyid(
+            {'username': updateDataReq["username"]})
 
         # if  user is not exist then skip the update user
         userlist = list(usercursor)
         if len(userlist) == 0:
             return Response(response="User not exist, please try again", status=200, mimetype="application/json")
 
-
         # pass the primary key as filter in order to update document based on it
-        filter = { 'username': updateDataReq["username"] }
-        print(filter)
+        filter = {'username': updateDataReq["username"]}
+        print("====filter====>", filter)
 
         # set what are the values need to update in document
         dataForUpdate = UserDetailsData.user_UpdateData(request)
         print("data :", dataForUpdate)
-        updatedData = { "$set": dataForUpdate }
+        updatedData = {"$set": dataForUpdate}
         UserDetails.update_user(filter, updatedData)
 
         print("*** After Update  *")
@@ -152,22 +149,22 @@ def updateUserByName():
         return Response(response="Update is Failed", status=500, mimetype="application/json")
 
 
-@app.route('/deleteUserByName', methods=['GET','POST'])
+@app.route('/deleteUserByName', methods=['GET', 'POST'])
 def deleteUserByName():
     try:
 
         print(request.json["username"])
         # find method returns cursor object
-        usercursor = UserDetails.get_userbyid({'username':request.json["username"]})
+        usercursor = UserDetails.get_userbyid(
+            {'username': request.json["username"]})
 
         # if  user is not exist then skip the delete user
         userlist = list(usercursor)
         if len(userlist) == 0:
             return Response(response="User not exist, please try again", status=200, mimetype="application/json")
-        
-        
+
         # pass the primary key data as filter to delete the document
-        filter = { 'username': request.json["username"] }
+        filter = {'username': request.json["username"]}
         print(filter)
         UserDetails.delete_user(filter)
 
@@ -178,7 +175,6 @@ def deleteUserByName():
         print("*Exception*")
         print(ex.__str__())
         return Response(response="Delete is Failed", status=500, mimetype="application/json")
-
 
 
 if __name__ == '__main__':
