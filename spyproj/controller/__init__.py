20ee11a80d11662e7269fb588ea0c83ed4d48ab8
Gopi <krishnar8@gmail.com>
2022-10-14 20:55:44 +0530

    commit

--- spyproj/controller/__init__.py
+++ spyproj/controller/__init__.py
@@ -2,5 +2,3 @@ from spyproj.controller import camera
 from spyproj.controller import historyroute
 from spyproj.controller import userroute
 from flask import Flask
-
-app = Flask(__name__)
--- spyproj/controller/camera.py
+++ spyproj/controller/camera.py
@@ -4,7 +4,7 @@ from spyproj import app
 from spyproj.repository.cameradetails_repository import CameraDetails
 from spyproj.model.camera_details_data import CameraDetailsData
 from flask import jsonify
-from spyproj.detect import detect
+#from spyproj.detect import detect
 
 
 @app.route('/cameras', methods=['GET'])
@@ -46,7 +46,7 @@ def cameras():
                     'cameraname': cameraname
                 }
                 cameras.append(dataDict)
-                detect()
+                # detect()
             # convert into json
             # json_data = json.dumps(xyz, default = str)
             json_data = jsonify(cameras)
