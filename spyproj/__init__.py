58048b551f532219dccdeff42be939dca1a6954a
kamalakumar02 <kamalakumar.m02@gmail.com>
2022-10-15 08:10:59 +0530

    base init commit

--- spyproj/__init__.py
+++ spyproj/__init__.py
@@ -1,6 +1,7 @@
-from spyproj import repository
-from spyproj import model
-from spyproj import controller
 from flask import Flask
 
 app = Flask(__name__)
+
+from spyproj import controller
+from spyproj import model
+from spyproj import repository
